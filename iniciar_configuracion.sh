23b3d67d64aa83be6e8859f0907e0c5d7ad57d12
Julio Cesar Cori Ochoa <ingeumsacori@gmail.com>
2021-09-07 10:49:34 -0400

    agregando perfil imagen

--- iniciar_configuracion.sh
+++ iniciar_configuracion.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 # ejecutar el archivo para realizar las configuraciones iniciales
 mkdir -p media
-cp -r archivos_defecto/firmas media/
-cp -r archivos_defecto/formularios media/
+cp -r archivos_defecto/firmas/ media/
+cp -r archivos_defecto/formularios/ media/
+cp -r archivos_defecto/imagene/s media/
 
