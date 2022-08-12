159d519e6d84ff9c772d719fb29dcd285f19cad0
Julio Cesar Cori Ochoa <ingeumsacori@gmail.com>
2022-08-12 10:59:09 -0400

    test: Se creo un test para modelo Actividad

--- actividades/tests.py
+++ actividades/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import *
 
-# Create your tests here.
+class ActividadTest(TestCase):
+    def create_actividad(self, nombre="estudiar reglamento", 
+            nombre_humano="Se debe estudiar el reglamento",
+            detalle="El estudiante debe estudiar el reglamento y confirmar su estudio",
+            valor=4,
+            orden=1):
+        return Actividad.objects.create(nombre=nombre,nombre_humano=nombre_humano,
+                detalle=detalle, valor=valor, orden=orden, fecha_creacion=timezone.now())
+            
+    def test_create_actividad(self):
+        w = self.create_actividad()
+        self.assertTrue(isinstance(w, Actividad))
