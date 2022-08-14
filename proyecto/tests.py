ed97332945d9bbaffcc22fed0f62762ddd0ebb7f
Julio Cesar Cori Ochoa <ingeumsacori@gmail.com>
2022-08-14 19:13:00 -0400

    test: datos tribunales

--- proyecto/tests.py
+++ proyecto/tests.py
@@ -56,3 +56,45 @@ class DatosDirectorTest(TestCase):
         )
 
         self.assertTrue(isinstance(w, DatosDirector))
+
+class DatosTribunalTest(TestCase):
+            
+    def test_datos_tribunal(self):
+        usuario = User.objects.create(first_name="Ricardo", last_name="Jordan Rodriguez")
+        mencion1 = Mencion.objects.create(nombre="Telecomunicación", fecha_creacion = timezone.now())
+        mencion2 = Mencion.objects.create(nombre="Sistemas de Control", fecha_creacion = timezone.now())
+        w = DatosTribunal.objects.create(
+            usuario = usuario,
+            correo = "test@gmail.com",
+            nombre = "Ricardo",
+            apellido = "Jordan Rodriguez",
+            celular = "73675872",
+            firma = 'firmas/firma_default.jpg',
+            imagen_perfil = "imagenes/profile1.png",
+            fecha_inscripcion = timezone.now(),
+        )
+        w.menciones.add(mencion1)
+        w.menciones.add(mencion2)
+
+        self.assertTrue(isinstance(w, DatosTribunal))
+
+class DatosTribunalTest(TestCase):
+            
+    def test_datos_tribunal(self):
+        usuario = User.objects.create(first_name="Ricardo", last_name="Jordan Rodriguez")
+        mencion1 = Mencion.objects.create(nombre="Telecomunicación", fecha_creacion = timezone.now())
+        mencion2 = Mencion.objects.create(nombre="Sistemas de Control", fecha_creacion = timezone.now())
+        w = DatosTribunal.objects.create(
+            usuario = usuario,
+            correo = "test@gmail.com",
+            nombre = "Ricardo",
+            apellido = "Jordan Rodriguez",
+            celular = "73675872",
+            firma = 'firmas/firma_default.jpg',
+            imagen_perfil = "imagenes/profile1.png",
+            fecha_inscripcion = timezone.now(),
+        )
+        w.menciones.add(mencion1)
+        w.menciones.add(mencion2)
+
+        self.assertTrue(isinstance(w, DatosTribunal))
