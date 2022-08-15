9686b71f32190672ff8d336b46bdd075ec9c7845
Julio Cesar Cori Ochoa <ingeumsacori@gmail.com>
2022-08-14 20:19:36 -0400

    test: historial actividades

--- actividades/tests.py
+++ actividades/tests.py
@@ -3,14 +3,33 @@ from django.utils import timezone
 from .models import *
 
 class ActividadTest(TestCase):
-    def create_actividad(self, nombre="estudiar reglamento", 
+    def create_actividad(self, 
+            nombre="estudiar reglamento", 
             nombre_humano="Se debe estudiar el reglamento",
             detalle="El estudiante debe estudiar el reglamento y confirmar su estudio",
             valor=4,
-            orden=1):
+            orden=1
+            ):
         return Actividad.objects.create(nombre=nombre,nombre_humano=nombre_humano,
                 detalle=detalle, valor=valor, orden=orden, fecha_creacion=timezone.now())
             
     def test_create_actividad(self):
         w = self.create_actividad()
         self.assertTrue(isinstance(w, Actividad))
+
+class ActividadHistorialTest(TestCase):
+    def test_create_actividad(self):
+        # no se necesito de Equipo. 
+        actividad = Actividad.objects.create(
+                nombre="estudiar reglamento", 
+                nombre_humano="Se debe estudiar el reglamento",
+                detalle="El estudiante debe estudiar el reglamento y confirmar su estudio",
+                valor=4,
+                orden=1,
+                )
+        w = ActividadHistorial.objects.create(
+                # equipo = equipo,
+                actividad = actividad,
+                fecha_creacion = timezone.now(),
+                )
+        self.assertTrue(isinstance(w, ActividadHistorial))
