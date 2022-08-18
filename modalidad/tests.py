dbed369254db86cbc6dd968c96fe5a98c29918c5
Julio Cesar Cori Ochoa <ingeumsacori@gmail.com>
2022-08-18 19:17:03 -0400

    test: modalidad

--- modalidad/tests.py
+++ modalidad/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.utils import timezone
+
+from .models import *
+
+class SolicitudTest(TestCase):
+            
+    def test_solicitud(self):
+        w = Solicitud.objects.create(
+            asunto = 'Solicitud de Modalidad Múltiple',
+            detalle = """El tutor me sugiere que el proyecto lo elaboren 2 personas.""",
+            visto = False,
+            aprobar = True,
+            fecha_creacion = timezone.now()
+        )
+
+        self.assertTrue(isinstance(w, Solicitud))
+
+class RechazarSolicitudTest(TestCase):
+            
+    def test_rechazar_solicitud(self):
+        w = RechazarSolicitud.objects.create(
+            asunto = 'Solicitud de Modalidad Múltiple',
+            detalle = """El tutor me sugiere que el proyecto lo elaboren 2 personas.""",
+            visto = False,
+            fecha_creacion = timezone.now()
+        )
+
+        self.assertTrue(isinstance(w, RechazarSolicitud))
+
+class SolicitudIntegranteTest(TestCase):
+            
+    def test_solicitud_integrante(self):
+        w = SolicitudIntegrante.objects.create(
+            correo_invitado = "ejemplo@gmail.com",
+            visto = False,
+            estado = "aprobar",
+            fecha_creacion = timezone.now()
+        )
+
+        self.assertTrue(isinstance(w, SolicitudIntegrante))
