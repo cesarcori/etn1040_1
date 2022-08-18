1a53130094a915a449444f922ce2c86fc02c154a
Julio Cesar Cori Ochoa <ingeumsacori@gmail.com>
2022-08-18 19:17:18 -0400

    test: reportes

--- reportes/tests.py
+++ reportes/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import *
+
+class TituloPerfilTest(TestCase):
+            
+    def test_titulo_perfil(self):
+        w = TituloPerfil.objects.create(
+            titulo = models.CharField(max_length=200, null=True, blank=True)
+        )
+
+        self.assertTrue(isinstance(w, TituloPerfil))
