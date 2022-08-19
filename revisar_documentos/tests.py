19efc0389b03c7958dca752d598b45adac913f4a
Julio Cesar Cori Ochoa <ingeumsacori@gmail.com>
2022-08-18 20:13:18 -0400

    test: revisar_documentos, model sala documentos doc

--- revisar_documentos/tests.py
+++ revisar_documentos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.utils import timezone
+
+from .models import *
+
+
+class SalaDocumentoDocTest(TestCase):
+    def test_sala_documento_doc(self):
+        w = SalaDocumentoDoc.objects.create(
+            visto_bueno = False,
+            tipo = "perfil",
+            updated = timezone.now(),
+            fecha_creacion = timezone.now(),
+        )
+
+        self.assertTrue(isinstance(w, SalaDocumentoDoc))
